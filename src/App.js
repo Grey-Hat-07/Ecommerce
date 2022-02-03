9438a8fc882ee5f69a8da9b08ccdd7f5e6b8e463
subhamay2002 <subhamaynath2002@gmail.com>
2022-02-03 11:10:50 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import React from 'react';
+import About from './components/About ';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     hello
+     <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" >
+          <About/>
+        </Route>
+      </Switch>
+      </Router>
+     
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default function Navbar() {
+  return <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <Link class="navbar-brand" to="/"> E-Commerce</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <Link to='/' className="nav-link">Home</Link>
+        </li>
+        <li class="nav-item">
+          <Link to='/about' className="nav-link">About</Link> 
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  </div>;
+}
